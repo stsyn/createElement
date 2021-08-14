f2b323e5adaa044429a5ad03b3cde3b7ce4803dc
cantusemyname <cantusemyname@yandex-team.ru>
2021-08-14 17:50:59 +0300

    basic component support

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # createElement
-Real-DOM dependencies-free imitator of React.createElement() designed to simplify creating HTMLElements from JavaScript code, when you for some reason don't want to use templates or frameworks.
+Real-DOM dependencies-free imitator of React.createElement() designed to simplify creating HTMLElements from JavaScript 
+code, when you for some reason don't want to use templates or frameworks.
 
-It's not designed to work with real-time "canvas-like" redrawing (render everything each X ms), this may significally worse responsibility of active UI elements (like inputs).
+It's not designed to work with real-time "canvas-like" redrawing (render everything each X ms), this may significally 
+worse responsibility of active UI elements (like inputs).
 
 # How to use
 Simply add `<script>` tag in your HTML.
@@ -9,26 +11,27 @@ Simply add `<script>` tag in your HTML.
 ## createElement(CSS selector, options, children)
 Similar with `React.createElement` syntax, but all child elements should be in array. 
 - ***returns*** — HTMLElement;
-- **CSS selector** — `tag#id.className`, empty tag defaults to `div`;
+- **CSS selector** — `tag#id.className` or **component**, empty tag defaults to `div`;
 - **options** — **[can be omited]** map-like object with general HTMLElement fields. Special cases:
-  - *style* — can be both string (then it will be applied as CSS-string), or object (then each property will be applied as JS-accesible field);
+  - *style* — can be both string (then it will be applied as CSS-string), or object (then each property will be applied 
+    as JS-accesible field);
   - *dataset* — allows to set `data-attributes` on the element;
   - *className* — combined with retrieved from **CSS selector**, recommeded for dynamic classes;
   - *id* — takes priority over retrieved from **CSS selector**, recommeded for dynamic IDs;
   - *onX* — events.
 - **children** — **[can be omited]** array of HTMLElements or any primitive types to insert into created element;
 
-***Note 1:*** keep in mind, that **you can't insert the same HTMLElement into multiple different places on the page**. This library makes a deep copy, when it detects not-empty parentNode property, but **it will be the copy from the current state of the element**. Avoid reusage of dynamically changable elements or consider *Notation mode*;
+***Note 1:*** keep in mind, that **you can't insert the same HTMLElement into multiple different places on the page**. 
+This library makes a deep copy, when it detects not-empty parentNode property, but **it will be the copy from the 
+current state of the element**. Avoid reusing of dynamically changing elements or consider *Notation mode* / *Components*;
 
 ***Note 2:*** single child element can be inserted without array;
 
-***Note 3:*** generally speaking any type is acceptable as child element, but **arrays are unacceptable**, since they will be confused with **Notation mode**. Theoretically you can still use common JS objects, which are not HTMLElements or arrays, but you should avoid doing this;
+***Note 3:*** generally speaking any type is acceptable as child element, but **arrays are unacceptable**, since they 
+will be confused with **Notation mode**. Theoretically you can still use common JS objects, which are not HTMLElements 
+or arrays, but you should avoid doing this;
 
 ***Note 4:*** `null` or `undefined` in children array won't be rendered.
-
-## createFromNotation(CSS selector, options, childElement1, childElement2...)
-**DEPRECATED, WILL BE REMOVED IN COMPONENTS VERSION**
-The same as `createElement`, but, as with `React.createElement`, all children should be defined as additional arguments.
 
 ## fillElement(container, children)
 Part of `createElement()`, inserts all `children` elements into `container` HTMLElement.
@@ -53,7 +56,10 @@ Inserts `container` in the place of `element` and puts `element` inside.
 - **element** — HTMLElement to be inserted inside of `container`.
 
 # Notation mode
-Initially designed to singifically reduce amount of code. You may omit function word and pass not HTMLElement as in  `createElement(CSS selector, options, [childElement1, childElement2...])`, but plain notation array `[CSS selector, options, [childElement1, childElement2...]]`. You can mix normal mode with Notation mode, when you need to store specific elements in variables.
+Initially designed to singifically reduce amount of code. You may omit function word and pass not HTMLElement as in 
+`createElement(CSS selector, options, [childElement1, childElement2...])`, but plain notation array 
+`[CSS selector, options, [childElement1, childElement2...]]`. You can mix normal mode with Notation mode, when you need 
+to store specific elements in variables.
 
 Consider two examples:
 
@@ -89,4 +95,6 @@ createElement('div', [
 ])
 ```
 
-***Note:*** While you can mix Notation mode with normal mode, you can't mix **definions** from `createElement` and `createFromNotation`. Format from the closest parent should be used.
+# Components
+You can use functions accepting `options` and `children` parameters instead of CSS selectors. Can return either a
+HTML element or *Notation*.
--- src/es5.js
+++ src/es5.js
@@ -1,194 +1,187 @@
-function __isElement(obj) {
-  try {
-    return obj instanceof HTMLElement;
-  }
-  catch (e) {
-    return typeof obj === "object" &&
-      obj.nodeType === 1 && typeof obj.style === "object" &&
-      typeof obj.ownerDocument ==="object";
-  }
-}
+// noinspection JSUnfilteredForInLoop
 
-function __createElement(tag, properties, children, notationCreator) {
-  var element = document.createElement(tag), i;
-  var applyAsJsField = ['innerHTML', 'checked', 'disabled', 'value', 'selected', 'className', 'crossOrigin', 'muted'];
-  var applyNotAsAttribute = applyAsJsField.concat(['events', 'dataset', 'style']);
+var ___temp___ = (function() {
+  function __isArray(a) {
+    return Array.isArray(a);
+  }
 
-  for (i in properties) {
-    if (i.indexOf('on') === 0) {
-      element.addEventListener(i.substring(2), properties[i]);
-    } else if (applyNotAsAttribute.indexOf(i) === -1) {
-      element.setAttribute(i, properties[i]);
-    } else if (applyAsJsField.indexOf(i) !== -1) {
-      element[i] = properties[i];
+  function __isElement(obj) {
+    try {
+      return obj instanceof HTMLElement;
+    } catch (e) {
+      return typeof obj === "object" &&
+        obj.nodeType === 1 && typeof obj.style === "object" &&
+        typeof obj.ownerDocument === "object";
     }
   }
 
-  if (typeof properties.dataset === 'object') {
-    for (i in properties.dataset) {
-      element.dataset[i] = properties.dataset[i];
+  function __createElement(tag, properties, children) {
+    if (__isElement(tag)) {
+      return tag;
     }
-  }
-  if (typeof properties.style === 'object') {
-    for (i in properties.style) {
-      element.style[i] = properties.style[i];
-    }
-  } else if (typeof properties.style === 'string') {
-    element.style = properties.style;
-  }
 
-  if (children && children.length) {
-    __fillElement(element, children, notationCreator);
-  }
-  return element;
-}
+    var element, i;
+    var applyAsJsField = ['innerHTML', 'checked', 'disabled', 'value', 'selected', 'className', 'crossOrigin', 'muted'];
+    var applyNotAsAttribute = applyAsJsField.concat(['events', 'dataset', 'style']);
 
-function __fillElement(element, items, notationCreator) {
-  var textCollection = null;
-  var isPreviousElementWasTextNode = false;
-
-  function makeTextNode() {
-    if (!textCollection) return;
-    element.appendChild(document.createTextNode(textCollection));
-    textCollection = null;
-    isPreviousElementWasTextNode = false;
-  }
-
-  for (var i = 0; i < items.length; i++) {
-    var c = items[i];
-    if (c === undefined || c === null) continue;
-    else if (Array.isArray(c)) {
-      if (isPreviousElementWasTextNode) {
-        makeTextNode();
+    element = document.createElement(tag);
+    for (i in properties) {
+      if (i.indexOf('on') === 0) {
+        element.addEventListener(i.substring(2), properties[i]);
+      } else if (applyNotAsAttribute.indexOf(i) === -1) {
+        element.setAttribute(i, properties[i]);
+      } else if (applyAsJsField.indexOf(i) !== -1) {
+        element[i] = properties[i];
       }
-      element.appendChild(notationCreator(c));
-    } else if (!__isElement(c)) {
-      if (textCollection === null) {
-        textCollection = String(c);
+    }
+
+    if (typeof properties.dataset === 'object') {
+      for (i in properties.dataset) {
+        element.dataset[i] = properties.dataset[i];
+      }
+    }
+    if (typeof properties.style === 'object') {
+      for (i in properties.style) {
+        element.style[i] = properties.style[i];
+      }
+    } else if (typeof properties.style === 'string') {
+      element.style = properties.style;
+    }
+
+    if (children && children.length) {
+      __fillElement(element, children);
+    }
+    return element;
+  }
+
+  function __fillElement(element, items) {
+    var textCollection = null;
+    var isPreviousElementTextNode = false;
+
+    function makeTextNode() {
+      if (!textCollection) return;
+      element.appendChild(document.createTextNode(textCollection));
+      textCollection = null;
+      isPreviousElementTextNode = false;
+    }
+
+    for (var i = 0; i < items.length; i++) {
+      var c = items[i];
+      if (c === undefined || c === null) {
+        continue;
+      }
+      if (__isArray(c)) {
+        if (isPreviousElementTextNode) {
+          makeTextNode();
+        }
+        var parsed = __repackAndValidate(c[0], c[1], c[2]);
+        element.appendChild(__createElement(parsed[0], parsed[1], parsed[2]));
+      } else if (!__isElement(c)) {
+        if (textCollection === null) {
+          textCollection = String(c);
+        } else {
+          textCollection += String(c);
+        }
+        isPreviousElementTextNode = true;
       } else {
-        textCollection += String(c);
+        if (isPreviousElementTextNode) {
+          makeTextNode();
+        }
+        element.appendChild(c.parentNode == null ? c : c.cloneNode(true));
       }
-      isPreviousElementWasTextNode = true;
-    } else {
-      if (isPreviousElementWasTextNode) {
-        makeTextNode();
+    }
+    makeTextNode();
+  }
+
+  function __repackAndValidate(tag, properties, children) {
+    if (__isElement(tag)) {
+      return [tag];
+    }
+    if (typeof tag !== 'string' && typeof tag !== 'function') {
+      throw new Error('Unacceptable CSS selector ' + tag);
+    }
+    if (__isElement(children) && !__isArray(children)) {
+      children = [children];
+    }
+
+    if (typeof properties === 'undefined') {
+      properties = {};
+    } else if (
+      __isElement(properties) ||
+      ['string', 'number', 'boolean'].indexOf(typeof properties) > -1
+    ) {
+      if (__isArray(children)) {
+        children.unshift(properties);
+      } else {
+        children = [properties];
       }
-      element.appendChild(c.parentNode == null ? c : c.cloneNode(true));
+      properties = {};
+    } else if (__isArray(properties)) {
+      if (__isArray(children)) {
+        children.concat(properties);
+      } else {
+        children = properties;
+      }
+      properties = {};
     }
-  }
-  makeTextNode();
-}
 
-function __notatorParser(children) {
-  var tag = children.shift();
-  var values = children.shift();
-  return __repackAndValidate(tag, values, children);
-}
+    if (typeof tag === 'string') {
+      var classes = (tag || '').split('.');
+      tag = classes.shift();
+      if (classes.length > 0) {
+        properties.className = (properties.className ? properties.className + ' ' : '') + classes.join(' ');
+      }
+      var id = tag.split('#');
+      tag = id.shift() || 'div';
+      id = id.shift();
 
-function __repackAndValidate(tag, properties, children) {
-  if (typeof tag !== 'string') {
-    console.trace();
-    throw 'Unacceptable CSS selector ' + tag;
-  }
-  if (__isElement(children) && !Array.isArray(children)){
-    children = [children];
-  }
-
-  if (typeof properties === 'undefined') {
-    properties = {};
-  } else if ((__isElement(properties) || typeof properties === 'string' || typeof properties === 'number' || typeof properties === 'boolean')) {
-    if (Array.isArray(children)) {
-      children.unshift(properties);
+      if (id && !properties.id) {
+        properties.id = id;
+      }
     } else {
-      children = [properties];
+      var data = tag(properties, children);
+      return __repackAndValidate(data[0], data[1], data[2]);
     }
-    properties = {};
-  } else if (Array.isArray(properties)) {
-    if (Array.isArray(children)) {
-      children.unshift(properties);
-    } else {
-      children = properties;
+
+    return [tag, properties, children];
+  }
+
+  function createElement(tag, properties, children) {
+    var items = __repackAndValidate(tag, properties, children);
+
+    return __createElement(items[0], items[1], items[2]);
+  }
+
+  function fillElement(element, items) {
+    if (!__isElement(element)) {
+      throw new Error('Unacceptable element ' + element);
     }
-    properties = {};
+    if (__isElement(items)) {
+      items = [items];
+    }
+    if (!__isArray(items)) {
+      throw new Error('Unacceptable children ' + items);
+    }
+
+    __fillElement(element, items);
   }
 
-  var classes = (tag || '').split('.');
-  tag = classes.shift();
-  if (classes.length > 0) {
-    properties.className = (properties.className ? properties.className + ' ' : '') + classes.join(' ');
-  }
-  var id = tag.split('#');
-  tag = id.shift() || 'div';
-  id = id.shift();
-
-  if (id && !properties.id) {
-    properties.id = id;
-  }
-  return [tag, properties, children];
-}
-
-function createElement(tag, properties, children) {
-  var items = __repackAndValidate(tag, properties, children);
-
-  return __createElement(items[0], items[1], items[2], function (notation) {
-    return createElement(notation[0], notation[1], notation[2]);
-  });
-}
-
-function createFromNotation(tag, properties, children) {
-  var parsed = __notatorParser(Array.prototype.slice.call(arguments));
-
-  return __createElement(parsed[0], parsed[1], parsed[2], function notatorParser(notation) {
-    var parsed = __notatorParser(notation);
-    return __createElement(parsed[0], parsed[1], parsed[2], notatorParser);
-  });
-}
-
-function fillElement(element, items) {
-  if (!__isElement(element)) {
-    console.trace();
-    throw 'Unacceptable element ' + String(element);
-  }
-  if (__isElement(items)) {
-    items = [items];
-  }
-  if (!Array.isArray(items)) {
-    console.trace();
-    throw 'Unacceptable children ' + String(items);
+  function wrapElement(element, target) {
+    var parent = target.parentNode;
+    parent.insertBefore(element, target);
+    element.appendChild(target);
   }
 
-  __fillElement(element, items, function (notation) {
-    return createElement(notation[0], notation[1], notation[2]);
-  });
-}
-
-function fillFromNotation(element, items) {
-  if (!__isElement(element)) {
-    console.trace();
-    throw 'Unacceptable element ' + String(element);
-  }
-  if (__isElement(items)) {
-    items = [items];
-  }
-  if (!Array.isArray(items)) {
-    console.trace();
-    throw 'Unacceptable children ' + String(items);
+  function clearElement(element) {
+    while (element.hasChildNodes()) {
+      element.removeChild(element.lastChild);
+    }
   }
 
-  __fillElement(element, items, function notatorParser(notation) {
-    var parsed = __notatorParser(notation);
-    return __createElement(parsed[0], parsed[1], parsed[2], notatorParser);
-  });
-}
+  return [createElement, fillElement, wrapElement, clearElement];
+})();
 
-function wrapElement(element, target) {
-  var parent = target.parentNode;
-  parent.insertBefore(element, target);
-  element.appendChild(target);
-}
-
-function clearElement(element) {
-  while (element.hasChildNodes()) {
-    element.removeChild(element.lastChild);
-  }
-}
+var createElement = ___temp___[0];
+var fillElement = ___temp___[1];
+var wrapElement = ___temp___[2];
+var clearElement = ___temp___[3];
