31fefa8c11571c474ff19daeb408ca4fd7f9f62c
stsyn <stasyan1902@gmail.com>
2021-12-18 11:51:55 +0300

    bidirectional value passing

--- src/extensions/forms.js
+++ src/extensions/forms.js
@@ -16,29 +16,62 @@ var __temp__ = (function() {
   }
 
   function __childIterator(data, onFormChange) {
-    return function(child) {
-      var oldOnChange = child[1] && child[1].onchange;
+    return function(_child) {
+      if (!_child || !Array.isArray(_child)) return _child;
+      var child = [_child[0], __mergeObjects({}, _child[1]), Array.isArray(_child[2]) ? [].concat(_child[2]) : _child[2]];
       var propName = child[1] && child[1].name;
 
       if (__notNull(propName)) {
+        var oldOnChange = child[1] && child[1].onchange;
+        var oldRerender = child[1] && child[1]._redraw;
+        var oldCast = child[1] && child[1]._cast;
+
         if (!__notNull(data[propName])) data[propName] = __notNull(child[1].value) ? child[1].value : '';
 
-        child[1].value = data[propName];
+        var renderFunc, element, elementValue = data[propName];
+
+        child[1].value = elementValue;
+
+        child[1]._redraw = function(rerenderFunc) {
+          renderFunc = rerenderFunc;
+          oldRerender && oldRerender(rerenderFunc);
+        }
+
+        child[1]._cast = function(elem) {
+          element = elem;
+        }
 
         child[1].onchange = function(event) {
-          var canProceed = !oldOnChange || oldOnChange(event);
+          var value = (element.tagName === 'INPUT' && element.type === 'checkbox') ? element.checked : element.value;
+          var canProceed = !oldOnChange || oldOnChange(event) !== false;
 
           if (canProceed) {
-            data[propName] = __notNull(event.target.checked) ? event.target.checked : event.target.value;
-            onFormChange && onFormChange();
+            elementValue = value;
+            onFormChange && onFormChange(data);
           }
         };
+
+        Object.defineProperty(data, propName, {
+          get: function() {
+            return elementValue;
+          },
+          set: function(newValue) {
+            if (element) {
+              (element.tagName === 'INPUT' && element.type === 'checkbox') ? (element.checked = newValue) : (element.value = newValue);
+            }
+            child[1].value = newValue;
+            elementValue = newValue;
+            renderFunc(child);
+          }
+        });
       }
 
-      var elementChildren = Array.isArray(child[1]) ? child[1] : Array.isArray(child[2]) ? child[2] : null;
+      if (child[0] === formElement) return child;
+
+      var elementChildren = Array.isArray(child[2]) ? child[2] : null;
 
       if (elementChildren) {
-        __iterateChildren(elementChildren, __notNull(propName) ? data[propName] : data, onFormChange);
+        child[2] = __iterateChildren(elementChildren, __notNull(propName) ? data[propName] : data, onFormChange);
       }
 
       return child;
